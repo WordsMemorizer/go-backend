f83f7f8256eb4078947f13a6e1e1d4561d423cd0
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2021-08-16 21:03:13 +0300

    Added stdout logger

---
+++ src/common/router/middlewares/StdoutLoggerMiddleware.go
@@ -0,0 +1,53 @@
+package middlewares
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type stdoutLoggerWriterProxy struct {
+	http.ResponseWriter
+	code int
+	data []byte
+}
+
+func (s *stdoutLoggerWriterProxy) WriteHeader(code int) {
+	s.code = code
+	s.ResponseWriter.WriteHeader(code)
+}
+
+func (s *stdoutLoggerWriterProxy) Write(data []byte) (int, error) {
+	if s.isError() {
+		s.data = data
+	}
+	return s.ResponseWriter.Write(data)
+}
+
+func (s *stdoutLoggerWriterProxy) isError() bool { return s.code >= 400 }
+
+// StdoutLoggerMiddleware is http.HandleFunc level proxy
+// it will print information about request and error (if response code is error code)
+var StdoutLoggerMiddleware = func(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy := &stdoutLoggerWriterProxy{w, 0, nil}
+
+		next(proxy, r)
+
+		req := fmt.Sprintf("%s %s -> %v; Headers: %v", r.Method, r.URL, proxy.code, r.Header)
+
+		if proxy.isError() {
+
+			errStr := ""
+
+			if len(proxy.data) != 0 {
+				errStr = string(proxy.data)
+			}
+
+			response := fmt.Sprintf("Headers: %v; Error: %s", w.Header(), errStr)
+
+			log.Println(req, response)
+		}
+		log.Println(req)
+	}
+}
